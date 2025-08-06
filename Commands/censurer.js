726008fc1cf6f8eceda6415cea293fc3dd0f6cb4
Satalis <satalis@hotmail.fr>
2025-08-06 12:52:48 +0200

    Update censurer.js

--- Commands/censurer.js
+++ Commands/censurer.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports = {
-    name: "censurer",
+    name: "Censurer",
     description: "Remplace un message par une version masquée en cas de spoil.",
     type: "MESSAGE",
     dm: false,
